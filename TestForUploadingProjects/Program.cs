79d7373207224bc9802e490b545e6bdef53ad05a
Yancho-BB <yancho.cs@gmail.com>
2018-03-13 10:21:17 +0200

    Message so I can skip hell

---
+++ TestForUploadingProjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestForUploadingProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            if (message.Length < 10)
+            {
+                Console.WriteLine("Length is bigger than 10");
+            }
+            else
+            {
+                Console.WriteLine("Length is less than 10");
+            }
+            Console.WriteLine("Made a change");
+        }
+    }
+}
